d438067f3c5929644a34548aa07800b63a286250
Artem Rakov <arakov@fastmail.com>
2021-10-11 02:31:29 +0000

    make sure that permittable first prepended

--- test/support/user.rb
+++ test/support/user.rb
@@ -2,10 +2,19 @@
 
 class User
   include ActiveModel::Model
-  attr_accessor :valid_attribute, :invalid_attribute
+  attr_accessor :valid_attribute, :invalid_attribute, :locale
 
   def update(*args)
     assign_attributes(*args)
     true
+  end
+
+  def initialize(attrs = nil)
+    defaults = {
+      locale: :en
+    }
+
+    attrs_with_defaults = attrs ? defaults.merge(attrs) : defaults
+    super(attrs_with_defaults)
   end
 end
